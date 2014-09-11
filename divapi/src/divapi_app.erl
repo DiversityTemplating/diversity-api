4f590fbcee9dad0bf1da2db15c0b4599efa32007
jonas hadin <jonas@textalk.se>
2014-09-11 15:15:18 +0200

    first route

--- divapi/src/divapi_app.erl
+++ divapi/src/divapi_app.erl
@@ -4,7 +4,15 @@
 -export([start/2]).
 -export([stop/1]).
 
+-define(PORT, 8181).
+
 start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", diversity_api_handler, []}]}
+    ]),
+    cowboy:start_http(my_http_listener, 100, [{port, ?PORT}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
 	divapi_sup:start_link().
 
 stop(_State) ->
