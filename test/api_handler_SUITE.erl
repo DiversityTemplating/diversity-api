f4a66080c3a705f5b06505167232469f5e46a95c
jonas hadin <jonas@textalk.se>
2014-09-12 13:54:22 +0200

    moved everything

---
+++ test/api_handler_SUITE.erl
@@ -0,0 +1,20 @@
+-module(api_handler_SUITE).
+-include_lib("common_test/include/ct.hrl").
+
+-export([all/0]).
+-export([basic_handle/1]).
+
+-record(state, {}).
+
+all() -> [basic_handle].
+
+basic_handle(_Config) ->
+
+    SimplifiedReq = {http_req,<<"GET">>,'HTTP/1.1'},
+    State = #state{},
+    meck:new(cowboy_req),
+    meck:expect(cowboy_req, reply, fun(200, Headers, Body, Req) -> {ok, Req} end),
+
+    Response = diversity_api_handler:handle(SimplifiedReq, State),
+    Response =:= {ok, SimplifiedReq, State},
+    meck:unload(cowboy_req).
