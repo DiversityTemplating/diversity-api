7ca06692206d972339c61ae228ee3eff27d7aa0c
jonas hadin <jonas@textalk.se>
2014-09-12 09:40:22 +0200

    travis stuff

--- divapi/src/divapi_app.erl
+++ divapi/src/divapi_app.erl
@@ -10,7 +10,7 @@ start(_Type, _Args) ->
     Dispatch = cowboy_router:compile([
         {'_', [{"/", diversity_api_handler, []}]}
     ]),
-    cowboy:start_http(my_http_listener, 100, [{port, ?PORT}],
+    cowboy:start_http(diversity_api_listener, 100, [{port, ?PORT}],
         [{env, [{dispatch, Dispatch}]}]
     ),
 	divapi_sup:start_link().
