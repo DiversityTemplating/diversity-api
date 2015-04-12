87cc2a4243b6e6fa7f72617d63cab2e2275520ae
Raoul Hess <raoul@Metis.local>
2015-04-12 22:11:43 +0200

    Preperation of files needed to minify

--- src/components_js_minifier.erl
+++ src/components_js_minifier.erl
@@ -13,13 +13,15 @@ init(_, Req, _Opts) ->
 
 handle(Req, State) ->
     %% Proplist with one or more components to minfify
-    {Components, Req1} = cowboy:qs_vals(Req),
+    {Components, Req1} = cowboy_req:qs_vals(Req),
 
     %% Build a map with all components like
-    ComponentsWithFiles = get_files_components(Components),
-
+    ScriptFilesMap = get_file_components(Components),
+    io:format("~p", [ScriptFilesMap]),
     %% maps:map through the entire map and minify each file an put'em together
-    minify_components_js(),
+    Result = minify_components_js(ScriptFilesMap),
+    Result,
+    cowboy_req:reply(200, [], <<"FOOO">>, Req1),
     {ok, Req, State}.
 
 terminate(_Reason, _Req, _State) ->
@@ -27,10 +29,29 @@ terminate(_Reason, _Req, _State) ->
 
 %% End %%
 
-%% Return #{components => {tag, [files]}}
-get_components_files(Components) ->
+get_file_components(Components) ->
+    get_components_files(Components, #{}).
 
+get_components_files([{Component, Tag} | Rest], Map) ->
+    {DiversityProps} = jiffy:decode(git_utils:get_diversity_json(Component, Tag)),
+    ScriptFiles = proplists:get_value(<<"script">>, DiversityProps),
+    TaggedScriptFiles = lists:map(fun get_file_type/1, ScriptFiles),
+    get_components_files(Rest, maps:put({Component, Tag}, TaggedScriptFiles, Map));
+get_components_files([], Map) ->
+    Map.
 
+get_file_type(<<"\\", _Rest/binary>> = ScriptFile) ->
+    %% This is an exteral js file that needs to be downloaded.
+    {external, ScriptFile};
+get_file_type(ScriptFile) ->
+    case re:run(ScriptFile, <<".min.js">>) of
+        {match, _} ->
+            %% It's already minified. And exists in our component already
+            {already_minified, ScriptFile};
+        _ ->
+            %% It's not a minified file. Assume internal
+            {file, ScriptFile}
+    end.
 
 %% @doc
 %% Go through each component/tag minify and concatenate result to one file.
